35099bc7ab2f929643135ed5309d3284d1bcaf88
josueprlt <josue.perrault@etu.unilim.fr>
2023-12-20 18:30:40 +0100

    début it4

--- 2eme sprint/main.js
+++ 2eme sprint/main.js
@@ -166,7 +166,7 @@ for (let intervenant of personnes) {
     test2[intervenant]["semestre" + i] = {};
 
     let ressources = [...new Set(semestreEvents.map(event => event.ressource))];
-    
+
     for (let res of ressources) {
       let ressourceEvents = semestreEvents.filter((event) => {
         return event.ressource === res;
@@ -185,29 +185,85 @@ for (let intervenant of personnes) {
           return event.type.includes("TP");
         })
       };
-      console.log(test2);
+      /* console.log(test2); */
     }
   }
 }
 
 
 function hourStart(para) {
-  let heure = para.start.toString();
-  let test = heure.slice(16, -42);
-  test = test.replaceAll(":", ".");
+  let test = para.start.getHours() + "." + para.start.getMinutes();
   test = test.replaceAll("30", "50");
   test = parseFloat(test);
   return test;
 }
 function hourEnd(para) {
-  let heure = para.end.toString();
-  let test = heure.slice(16, -42);
-  test = test.replaceAll(":", ".");
+  let test = para.end.getHours() + "." + para.end.getMinutes();
   test = test.replaceAll("30", "50");
   test = parseFloat(test);
   return test;
 }
 
+
+/**
+* hour : un entier entre 0 et 23 qui détermine une tranche d'une heure (ex: 14 pour la tranche 14h-15h)
+* start : un objet Date qui détermine l'heure de début d'un cours
+* end : un objet Date qui détermine l'heure de fin d'un cours
+* retourne la durée de l'intersection entre [start, end] et [hour, hour+1] en heures (0 si pas d'intersection)
+*/
+let intersectByHour = function (hour, start, end) {
+
+  let interStart = new Date(start);
+  interStart.setHours(hour);
+  interStart.setMinutes(0);
+  let interEnd = new Date(end);
+  interEnd.setHours(hour + 1);
+  interEnd.setMinutes(0);
+
+  // maintenant il faut déterminer s'il existe une intersection entre [interStart, interEnd] et les horaires du cours [start, end]
+
+  if (interEnd <= start) // si l'heure de fin est avant l'heure de début du cours, pas d'intersection
+    return 0;
+  else if (interStart >= end) // si l'heure de début est après l'heure de fin du cours, pas d'intersection
+    return 0;
+  else { // il existe une intersection entre [interStart, interEnd] et les horaires du cours [start, end]
+    return (Math.min(interEnd, end) - Math.max(interStart, start)) / 1000 / 3600;
+    // on prend le minimum des deux heures de fin et le maximum des deux heures de début
+    // et on retourne la durée de l'intersection en heures
+  }
+
+}
+
+
+let test3 = {};
+let euhf = [];
+let profevent;
+
+for (let intervenant of personnes) {
+  euhf = 0
+
+  test3[intervenant] = {};
+  profevent = all.filter((event) => {
+    return event.title.includes(intervenant);
+  });
+
+  console.log(profevent[0]);
+
+  /* for (const elt of profevent) {
+    
+    for (let i = 1; i < 12; i++) {
+      console.log(intersectByHour(i, elt.start, elt.end));
+    }
+    
+  } */
+  
+}
+
+/* for (const ev of MmiAll) {
+  console.log(ev);
+  console.log(ev.start.getHours());
+  console.log(intersectByHour(ev.start.getHours(), ev.start, ev.end));
+} */
 
 
 
@@ -397,6 +453,167 @@ am5.ready(function () {
   chart.appear(1000, 100);
 
 }); // end am5.ready()
+
+
+
+am5.ready(function() {
+
+
+  // Create root element
+  // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+  var root = am5.Root.new("heatmap");
+  
+  
+  // Set themes
+  // https://www.amcharts.com/docs/v5/concepts/themes/
+  root.setThemes([
+    am5themes_Animated.new(root)
+  ]);
+  
+  
+  // Create chart
+  // https://www.amcharts.com/docs/v5/charts/xy-chart/
+  var chart = root.container.children.push(am5xy.XYChart.new(root, {
+    panX: false,
+    panY: false,
+    wheelX: "none",
+    wheelY: "none",
+    paddingLeft: 0,
+    layout: root.verticalLayout
+  }));
+  
+  
+  // Create axes and their renderers
+  var yRenderer = am5xy.AxisRendererY.new(root, {
+    visible: false,
+    minGridDistance: 20,
+    inversed: true,
+    minorGridEnabled: true
+  });
+  
+  yRenderer.grid.template.set("visible", false);
+  
+  var yAxis = chart.yAxes.push(am5xy.CategoryAxis.new(root, {
+    maxDeviation: 0,
+    renderer: yRenderer,
+    categoryField: "weekday"
+  }));
+  
+  var xRenderer = am5xy.AxisRendererX.new(root, {
+    visible: false,
+    minGridDistance: 30,
+    opposite:true,
+    minorGridEnabled: true
+  });
+  
+  xRenderer.grid.template.set("visible", false);
+  
+  var xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+    renderer: xRenderer,
+    categoryField: "hour"
+  }));
+  
+  
+  // Create series
+  // https://www.amcharts.com/docs/v5/charts/xy-chart/#Adding_series
+  var series = chart.series.push(am5xy.ColumnSeries.new(root, {
+    calculateAggregates: true,
+    stroke: am5.color(0xffffff),
+    clustered: false,
+    xAxis: xAxis,
+    yAxis: yAxis,
+    categoryXField: "hour",
+    categoryYField: "weekday",
+    valueField: "value"
+  }));
+  
+  series.columns.template.setAll({
+    tooltipText: "{value}",
+    strokeOpacity: 1,
+    strokeWidth: 2,
+    width: am5.percent(100),
+    height: am5.percent(100)
+  });
+  
+  series.columns.template.events.on("pointerover", function(event) {
+    var di = event.target.dataItem;
+    if (di) {
+      heatLegend.showValue(di.get("value", 0));
+    }
+  });
+  
+  series.events.on("datavalidated", function() {
+    heatLegend.set("startValue", series.getPrivate("valueHigh"));
+    heatLegend.set("endValue", series.getPrivate("valueLow"));
+  });
+  
+  
+  // Set up heat rules
+  // https://www.amcharts.com/docs/v5/concepts/settings/heat-rules/
+  series.set("heatRules", [{
+    target: series.columns.template,
+    min: am5.color(0xfffb77),
+    max: am5.color(0xfe131a),
+    dataField: "value",
+    key: "fill"
+  }]);
+  
+  
+  // Add heat legend
+  // https://www.amcharts.com/docs/v5/concepts/legend/heat-legend/
+  var heatLegend = chart.bottomAxesContainer.children.push(am5.HeatLegend.new(root, {
+    orientation: "horizontal",
+    endColor: am5.color(0xfffb77),
+    startColor: am5.color(0xfe131a)
+  }));
+  
+  
+  // Set data
+  // https://www.amcharts.com/docs/v5/charts/xy-chart/#Setting_data
+  var data = [{
+    hour: "9h",
+    weekday: "Lundi",
+    value: 2990
+  }, {
+    hour: "9h",
+    weekday: "Mardi",
+    value: 2520
+  }, {
+    hour: "10h",
+    weekday: "Mercredi",
+    value: 2334
+  }]
+  
+  series.data.setAll(data);
+  
+  yAxis.data.setAll([
+    { weekday: "Lundi" },
+    { weekday: "Mardi" },
+    { weekday: "Mercredi" },
+    { weekday: "Jeudi" },
+    { weekday: "Vendredi" }
+  ]);
+  
+  xAxis.data.setAll([
+    { hour: "8h" },
+    { hour: "9h" },
+    { hour: "10h" },
+    { hour: "11h" },
+    { hour: "12h" },
+    { hour: "13h" },
+    { hour: "14h" },
+    { hour: "15h" },
+    { hour: "16h" },
+    { hour: "17h" },
+    { hour: "18h" },
+    { hour: "19h" },
+  ]);
+  
+  // Make stuff animate on load
+  // https://www.amcharts.com/docs/v5/concepts/animations/#Initial_animation
+  chart.appear(1000, 100);
+  
+  });
 
 
 let data2 = [];
