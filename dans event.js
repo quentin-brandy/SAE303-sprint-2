a74bcb899ced8fa1f38ec874a85a5cdd9c2bd5ee
thechewgum <quentin.brandy@protonmail.com>
2023-12-21 14:22:07 +0100

    main

--- dans event.js
+++
@@ -1,9 +0,0 @@
-if (this.#summary.includes("CM")) {
-            this.#type = "CM";
-        } else if (this.#summary.includes("TD")) {
-            this.#type = "TD";
-        } else if (this.#summary.includes("TP")) {
-            this.#type = "TP";
-        } else {
-            this.#type = "OTHER";
-        }
